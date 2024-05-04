37f1dd03a8a1ac0d8b117ce71a92477bdadb8524
jsjhyun <gusrnl98@naver.com>
2024-05-04 19:21:51 +0900

    fix: 응답 수정

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,6 +1,9 @@
+from openai import OpenAI
 import streamlit as st
 import tiktoken
 from loguru import logger
+# from retriever import rag_func
+
 from langchain_openai import ChatOpenAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from openai import OpenAI
@@ -13,35 +16,20 @@ from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain_community.chat_message_histories.streamlit import StreamlitChatMessageHistory
 # from chatbot.main import get_conversation_chain, get_text, get_text_chunks, get_vectorstore
 from langchain_community.vectorstores import FAISS
-from langchain_core.messages import ChatMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from dotenv import load_dotenv
-load_dotenv()
-# from langchain.callbacks import get_openai_callback
-from streamlit import write
+
 
 def main():
     st.set_page_config(
     page_title="법률 상담 챗봇",
     page_icon=":books:")
 
-
     st.title("💬 법률 상담 챗봇")
     st.caption("쉽고, 편리한 법률 상담")
 
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
-
-    if "processComplete" not in st.session_state:
-        st.session_state.processComplete = None
-    #sidebar에 OpenAI API key를 입력받는 코드
     with st.sidebar:
         uploaded_files =  st.file_uploader("파일을 올려주세요.",type=['pdf','docx'],accept_multiple_files=True)
         openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-        process = st.button("Process")
+        # process = st.button("Process")
         "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"  
         st.markdown("---")
         st.markdown(
@@ -50,117 +38,30 @@ def main():
             "1. pdf, docx, txt 파일을 올려 사용할 수 있습니다.\n"
             "2. 채팅을 이용하여 법률 상담을 진행하세요.\n"
         ) 
-
         st.markdown("---")
         st.markdown("# About")
         st.markdown(
             "📖 챗봇을 사용하여 문서에 대해 질문하고 즉각적이고 정확한 답변을 얻을 수 있습니다. "
         )
 
-    if process:
-        if not openai_api_key:
-            st.info("알맞은 OpenAI API key를 입력해주세요.")
-            st.stop()
-        files_text = get_text(uploaded_files)
-        text_chunks = get_text_chunks(files_text)
-        vetorestore = get_vectorstore(text_chunks)
-     
-        st.session_state.conversation = get_conversation_chain(vetorestore,openai_api_key) 
-
-        st.session_state.processComplete = True
-
     if "messages" not in st.session_state:
         st.session_state["messages"] = [{"role": "assistant", "content": "안녕하세요! 법률 고민이 있으면 언제든 물어봐주세요!"}]
-    # 이전 대화 기록을 출력해주는 코드
-    if "messages" in st.session_state and len(st.session_state.messages) > 0:
-        for role, message in st.session_state["messages"]:
-            st.chat_message(role).write(message)
 
-    for msg in st.session_state["messages"]:
+    for msg in st.session_state.messages:
         st.chat_message(msg["role"]).write(msg["content"])
 
-
-    history = StreamlitChatMessageHistory(key="chat_messages")
-
-
-# Chat Logic
-    if user_input := st.chat_input("고민 있는 법률 문제를 입력해주세요!"):
-        st.chat_message("user").write(user_input)
-
+    if prompt := st.chat_input():
         if not openai_api_key:
-            st.info("OpenAI API key를 입력해주세요.")
+            st.info("Please add your OpenAI API key to continue.")
             st.stop()
-# OpenAI API 호출
-        client = OpenAI(api_key=openai_api_key)
-        st.session_state.messages.append({"role": "user", "content": user_input})
 
-       #AI 응답을 받아오는 코드
-        response = client.chat.completions.create(model="gpt-4", messages=st.session_state.messages)
+        client = OpenAI(api_key=openai_api_key)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+        response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
         msg = response.choices[0].message.content
-        with st.chat_message("assistant"):
-            write("상담 내용: " + user_input)
-        #AI 응답을 출력해주는 코드
         st.session_state.messages.append({"role": "assistant", "content": msg})
         st.chat_message("assistant").write(msg)
-
-def tiktoken_len(text):
-    tokenizer = tiktoken.get_encoding("cl100k_base")
-    tokens = tokenizer.encode(text)
-    return len(tokens)
-
-def get_text(docs):
-
-    doc_list = []
-    
-    for doc in docs:
-        file_name = doc.name  # doc 객체의 이름을 파일 이름으로 사용
-        with open(file_name, "wb") as file:  # 파일을 doc.name으로 저장
-            file.write(doc.getvalue())
-            logger.info(f"Uploaded {file_name}")
-        if '.pdf' in doc.name:
-            loader = PyPDFLoader(file_name)
-            documents = loader.load_and_split()
-        elif '.docx' in doc.name:
-            loader = Docx2txtLoader(file_name)
-            documents = loader.load_and_split()
-        elif '.pptx' in doc.name:
-            loader = UnstructuredPowerPointLoader(file_name)
-            documents = loader.load_and_split()
-
-        doc_list.extend(documents)
-    return doc_list
-
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=900,
-        chunk_overlap=100,
-        length_function=tiktoken_len
-    )
-    chunks = text_splitter.split_documents(text)
-    return chunks
-
-def get_vectorstore(text_chunks):
-    embeddings = HuggingFaceEmbeddings(
-                                        model_name="jhgan/ko-sroberta-multitask",
-                                        model_kwargs={'device': 'cpu'},
-                                        encode_kwargs={'normalize_embeddings': True}
-                                        )  
-    vectordb = FAISS.from_documents(text_chunks, embeddings)
-    return vectordb
-
-def get_conversation_chain(vetorestore,openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-            llm=llm, 
-            chain_type="stuff", 
-            retriever=vetorestore.as_retriever(search_type = 'mmr', vervose = True), 
-            memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
-            get_chat_history=lambda h: h,
-            return_source_documents=True,
-            verbose = True
-        )
-
-    return conversation_chain
 
 if __name__ == '__main__':
     main()
