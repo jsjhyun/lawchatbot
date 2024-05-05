0dfd0030533ecd381114a812d09061aeff25f3b8
gwanhwi <rhksgnl3@naver.com>
2024-05-05 19:20:57 +0900

    refactor: 리팩토링 및 content출력 수정

--- chatbot/main.py
+++ chatbot/main.py
@@ -19,6 +19,8 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.callbacks.manager import get_openai_callback
 from langchain_core.runnables import RunnablePassthrough
 from prompt import get_prompt
+from retriever import *
+
 def main():
     st.set_page_config(
     page_title="법률 상담 챗봇",
@@ -34,7 +36,7 @@ def main():
         st.session_state.chat_history = None
 
     with st.sidebar:      
-        uploaded_files =  st.file_uploader("파일을 올려주세요.",type=['pdf','docx'],accept_multiple_files=True)
+        #uploaded_files =  st.file_uploader("파일을 올려주세요.",type=['pdf','docx'],accept_multiple_files=True)
         openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         process = st.button("Process")
         "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)" 
@@ -58,12 +60,11 @@ def main():
         if not openai_api_key:
             st.info("Please add your OpenAI API key to continue.")
             st.stop()
-        files_text = get_text(uploaded_files)
+        files_text = get_pdf("/Users/eungwanhwi/Desktop/lawchatbotproject/venv/lawchatbot/chatbot/criminal_law.pdf")
         text_chunks = get_text_chunks(files_text)
         vetorestore = get_vectorstore(text_chunks)
         
         st.session_state.conversation = get_conversation_chain(vetorestore,openai_api_key) 
-
 #            st.session_state.processComplete = True 
 
     if "messages" not in st.session_state:
@@ -98,7 +99,7 @@ def main():
             chain = st.session_state.conversation
 
             with st.spinner("Thinking..."):
-                result = chain.invoke(user_input)
+                result = chain.invoke(user_input).content
                 #with get_openai_callback() as cb:
                     #st.session_state.chat_history = result['chat_history']
                 #response = result['answer']
@@ -108,60 +109,6 @@ def main():
 # Add assistant message to chat history
         st.session_state.messages.append({"role": "assistant", "content": result})
 
-def tiktoken_len(text):
-    tokenizer = tiktoken.get_encoding("cl100k_base")
-    tokens = tokenizer.encode(text)
-    return len(tokens)
-
-def get_text(docs):
-
-    doc_list = []
-    
-    for doc in docs:
-        file_name = doc.name  # doc 객체의 이름을 파일 이름으로 사용
-        with open(file_name, "wb") as file:  # 파일을 doc.name으로 저장
-            file.write(doc.getvalue())
-            logger.info(f"Uploaded {file_name}")
-        if '.pdf' in doc.name:
-            loader = PyPDFLoader(file_name)
-            documents = loader.load_and_split()
-        elif '.docx' in doc.name:
-            loader = Docx2txtLoader(file_name)
-            documents = loader.load_and_split()
-        elif '.pptx' in doc.name:
-            loader = UnstructuredPowerPointLoader(file_name)
-            documents = loader.load_and_split()
-
-        doc_list.extend(documents)
-    return doc_list
-
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=900,
-        chunk_overlap=100,
-        length_function=tiktoken_len
-    )
-    chunks = text_splitter.split_documents(text)
-    return chunks
-
-
-def get_vectorstore(text_chunks):
-    embeddings = HuggingFaceEmbeddings(
-                                        model_name="jhgan/ko-sroberta-multitask",
-                                        model_kwargs={'device': 'cpu'},
-                                        encode_kwargs={'normalize_embeddings': True}
-                                        )  
-    vectordb = FAISS.from_documents(text_chunks, embeddings)
-    return vectordb
-
-def get_conversation_chain(vetorestore,openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-4',temperature=0)
-    conversation_chain = (
-        {"context": vetorestore.as_retriever() , "question": RunnablePassthrough()}
-        | get_prompt()
-        | llm
-        )
-    return conversation_chain
 
 
 if __name__ == '__main__':
