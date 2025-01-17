7c65cf1ca981414bd87c6cfb93297f8203497009
Jeong_Hyun <105183327+jsjhyun@users.noreply.github.com>
2025-01-17 20:48:28 +0900

    Update FAQ.py

--- chatbot/pages/FAQ.py
+++ chatbot/pages/FAQ.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-#IFRAME = '<iframe src="https://ghbtns.com/github-btn.html?user=IvanIsCoding&repo=ResuLLMe&type=star&count=true&size=large" frameborder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>'
-
 st.markdown(
     f"""
     # 자주하는 질문 FAQ 
